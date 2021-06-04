5e946bff561e2f91011d0cf1bfb40501cda0f104
Suraj Kumar Chowdhry <srjchowdhry4@gmail.com>
2021-06-05 01:57:44 +0530

    changing route file

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Route::resource('celebrities', CelebrityController::class);
+Route::resource('/', CelebrityController::class);
